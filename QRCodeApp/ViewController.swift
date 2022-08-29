060cd3eadf3075021a63f12b8dea5a06af9f712d
Manohin <informmen@gmail.com>
2022-08-29 23:07:33 +0300

    Initial Commit

---
+++ QRCodeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeApp
+//
+//  Created by Alexey Manokhin on 29.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
